35ec5c002aa72c2a4893cbbd9babe106c74a5cb0
Tyler <47286889+tapevans@users.noreply.github.com>
2023-02-17 16:36:33 -0700

    0_25_SS

--- Model/KalmanFilter/CreateSSData.m
+++ Model/KalmanFilter/CreateSSData.m
@@ -10,12 +10,12 @@ T_s_max =  1; % T_s = 10^(T_s_max), **Keep this fix for now
 Ts_vec = logspace(T_s_min,T_s_max,N_t_s);
 
 % Split up like this for Github pushing
-% SOC_vec = 0:1:25; 
+SOC_vec = 0:1:25; 
 % SOC_vec = 26:1:49;
 % SOC_vec = 51:1:75;
 % SOC_vec = 76:1:100;
 
-SOC_vec = 50;
+% SOC_vec = 50;
 
 for SS = 1:length(SOC_vec)
     SOC = SOC_vec(SS);
